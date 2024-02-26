18a73679566103c823e1bf8d5dba914a56037e86
nurho <willowisawisp@gmail.com>
2024-02-26 16:11:43 +0000

    Fixed swap

--- configuration.nix
+++ configuration.nix
@@ -228,6 +228,7 @@
         zathura
         gimp
         pcmanfm
+        gparted
 
         # Fonts
         nerdfonts
--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -25,9 +25,13 @@
       fsType = "vfat";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/6e699b1b-111b-46e4-aeb7-909f0f508e54"; }
-    ];
+#  swapDevices =
+#    [ { device = "/dev/disk/by-uuid/6e699b1b-111b-46e4-aeb7-909f0f508e54"; }
+#    ];
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 16*1024;
+  } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
