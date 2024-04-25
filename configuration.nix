8268fcb4984da2746db9bf7b54648e330cc45042
nurho <12024497+nurho@users.noreply.github.com>
2024-04-25 13:32:30 +0100

    Set up some gnome settings

--- configuration.nix
+++ configuration.nix
@@ -19,9 +19,10 @@
   hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
 
-  # Locale
+  # locale
   time.timeZone = "Europe/London";
   i18n.defaultLocale = "en_GB.UTF-8";
+  services.xserver.xkb.layout = "gb";
   console.keyMap = "uk";
 
   # Graphics
